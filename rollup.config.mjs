2606f2d73492341646a382a4b10cbe1d890c18b0
yetanothervan <yetanothervan@gmail.com>
2025-05-10 03:36:43 +0300

    upd tailwind part 1

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,7 @@
 import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
 import svgr from '@svgr/rollup';
-import packageJson from "./package.json" assert { type: "json" };
+import packageJson from "./package.json" with { type: "json" };
 import dts from "rollup-plugin-dts";
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import { babel } from '@rollup/plugin-babel';
